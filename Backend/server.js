ffbe0248548e004e3b9ec3d0deb4b15674af9056
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-06-21 15:53:31 +0700

    add api for create routes

--- Backend/server.js
+++ Backend/server.js
@@ -29,3 +29,61 @@ db.connect((err) => {
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
   });
+
+
+
+// Create Routes
+app.post("/create", async (req, res) => {
+    const {
+      AccountID,
+      CustomerCode,
+      CompanyName,
+      CompanyAddress1,
+      CompanyAddress2,
+      ContactPerson,
+      Mobile,
+      Email,
+      TaxID,
+      BillingCharge,
+      AccountStatus,
+      DateModify,
+      ModifiedBy,
+      DateCreated,
+      CreatedBy,
+    } = req.body;
+    try {
+      db.query(
+        "INSERT INTO Accounts (AccountID, CustomerCode, CompanyName, CompanyAddress1, CompanyAddress2, ContactPerson, Mobile, Email, TaxID, BillingCharge, AccountStatus, DateModify, ModifiedBy, DateCreated, CreatedBy) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )",
+        [
+          AccountID,
+          CustomerCode,
+          CompanyName,
+          CompanyAddress1,
+          CompanyAddress2,
+          ContactPerson,
+          Mobile,
+          Email,
+          TaxID,
+          BillingCharge,
+          AccountStatus,
+          DateModify,
+          ModifiedBy,
+          DateCreated,
+          CreatedBy,
+        ],
+        (err, result, fields) => {
+          if (err) {
+            console.log("Error inserting into database", err);
+            return res.status(400).send();
+          }
+          return res
+            .status(201)
+            .json({ message: "Inserting into database is successful" });
+        }
+      );
+    } catch (err) {
+      console.log("Error inserting into database", err);
+      return res.status(500).send();
+    }
+  });
+  

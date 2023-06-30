ec246924fafaacf5779e359148c1312977399b05
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-06-30 15:21:53 +0700

    alert when data insert it dupicate

--- Frontend/src/services/AccountApi.jsx
+++ Frontend/src/services/AccountApi.jsx
@@ -12,22 +12,35 @@ export const AllInformation = async () => {
   }
 };
 
+// Function to check for duplicate data
+export const CheckDuplicateData = async (data) => {
+  const duplicateURL = `http://localhost:${port}/check-duplicate`;
+  const dataFormat = JSON.stringify(data);
+
+  try {
+    const response = await axios.post(duplicateURL, dataFormat, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response)
+    return response.data.duplicate; 
+  } catch (error) {
+    console.log("Failed to check duplicate data", error);
+    return false;
+  }
+};
+
+
 export const CreateInformation = async (data) => {
   // Define the data to be sent in the request body
 
   const baseURL = `http://localhost:${port}/create`;
   var data_format = JSON.stringify(data);
 
-
   if (!data.AccountID || !data.CustomerCode || !data.CompanyName) {
     console.log("Missing required field(s)");
-    return false;
-  }
-
-  // Check for duplicate data before inserting
-  const duplicateData = await checkDuplicateData(data);
-  if (duplicateData) {
-    console.log("Duplicate data found");
     return false;
   }
 
@@ -41,9 +54,8 @@ export const CreateInformation = async (data) => {
     });
     if (response.status === 201) {
       return true;
-    }
-    else if ( response.status === 400){
-      console.log(response.data)
+    } else if (response.status === 400) {
+      console.log(response.data);
     }
     return false;
   } catch (e) {
@@ -69,20 +81,6 @@ export const EditTnformation = async (user_id, data) => {
     return false;
   } catch (e) {
     console.log("Failed to fail data", e);
-    return false;
-  }
-};
-
-// Function to check for duplicate data
-const checkDuplicateData = async (data) => {
-  const duplicateURL = "http://localhost:3002/check-duplicate";
-  const dataFormat = JSON.stringify(data);
-
-  try {
-    const response = await axios.post(duplicateURL, dataFormat);
-    return response.data.duplicate; // Assuming the response contains a property indicating duplicate data
-  } catch (error) {
-    console.log("Failed to check duplicate data", error);
     return false;
   }
 };
